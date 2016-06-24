08c304dbb6e3cdc9726fa466565b44c6fbae1a37
Sairam <sairamch04@gmail.com>
2016-06-24 13:21:35 +0530

    Added toString,hashcode methods

--- src/main/java/com/FoodyBuddy/Model/Order.java
+++ src/main/java/com/FoodyBuddy/Model/Order.java
@@ -96,6 +96,7 @@ public class Order{
 		this.createdAt = createdAt;
 	}
 
+	
 	public Date getUpdatedAt() {
 		return updatedAt;
 	}
@@ -111,6 +112,41 @@ public class Order{
 	public void setDeletedAt(Date deletedAt) {
 		this.deletedAt = deletedAt;
 	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + netOrderAmount;
+		result = prime * result + ((status == null) ? 0 : status.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (netOrderAmount != other.netOrderAmount)
+			return false;
+		if (status == null) {
+			if (other.status != null)
+				return false;
+		} else if (!status.equals(other.status))
+			return false;
+		return true;
+	}
+	
 
 	
 }
