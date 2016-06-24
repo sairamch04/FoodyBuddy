24a0ca5b50f71de681a04a30e8bd3c96139f0ef0
Tanmay <tanmaykacker40@gmail.com>
2016-06-24 20:02:20 +0530

    Updated DAO and DAO Implementations
    Classes Included
    - Buyer
    - Country
    - State
    - City
    - Locality
    - Apartment

--- src/main/java/com/FoodyBuddy/Dao/CountryDAO.java
+++ src/main/java/com/FoodyBuddy/Dao/CountryDAO.java
@@ -1,16 +1,12 @@
-package com.FoodyBuddy.Dao;
+package com.FoodyBuddy.DAO;
 
 import java.util.List;
-
 import com.FoodyBuddy.Model.Country;
-import com.FoodyBuddy.Model.Seller;
 
 public interface CountryDAO {
-
-	public void save(Country c);
-	
-	public List<Country> list();
-	
-	public void update(Country c);
-	public void delete(Integer id);
+	public List<Country> getAllCountries();
+	public Country getCountry(int countryId);
+	public void insertCountry(Country country);
+	public void updateCountry(Country country);
+	public void deleteCountry(int id);
 }
