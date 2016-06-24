d768da5f5e7e5dcfd42135b21b7d8aec6c0f6952
sanjanagyl <sanjanagoyal21@gmail.com>
2016-06-24 18:54:11 +0530

    Update SellerDAOImpl.java

--- src/main/java/com/FoodyBuddy/Dao/Impl/SellerDAOImpl.java
+++ src/main/java/com/FoodyBuddy/Dao/Impl/SellerDAOImpl.java
@@ -1,7 +1,6 @@
 package com.FoodyBuddy.Dao.Impl;
 
 import java.util.List;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -14,7 +13,7 @@ public class SellerDAOImpl implements SellerDAO {
 
 	private SessionFactory sessionFactory;
 
-    public void setSessionFactory(SessionFactory sessionFactory) {
+    public SellerDAOImpl(SessionFactory sessionFactory) {
         this.sessionFactory = sessionFactory;
     }
     
@@ -25,23 +24,21 @@ public class SellerDAOImpl implements SellerDAO {
 		tx.commit();
 		session.close();
 	}
-	
-	@SuppressWarnings("unchecked")
-	public List<Seller> list() {
-		Session session = this.sessionFactory.openSession();
-		List<Seller> SellerList = session.createQuery("from Seller").list();
-		session.close();
-		return SellerList;
-	}
 
-	public void update(Seller s) {
-		Session session = this.sessionFactory.openSession();
-		Transaction tx = session.beginTransaction();
-		session.update(s);
-		tx.commit();
-		session.close();
-		
-	}
+	public void update(Seller seller )
+		 {
+		      Session session = sessionFactory.openSession();
+		      Transaction tx = null;
+		      try{
+		         tx = session.beginTransaction();
+				 session.update(seller); 
+		         tx.commit();
+		      }
+		      finally {
+		         session.close(); 
+		      }
+		 }
+
 
 	public void delete(Integer id) {
 		Session session = this.sessionFactory.getCurrentSession();
@@ -51,5 +48,30 @@ public class SellerDAOImpl implements SellerDAO {
 			session.delete(seller);
 		}
 	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Seller> listseller() {
+		Session session = this.sessionFactory.openSession();
+		List<Seller> SellerList = session.createQuery("from Seller").list();
+		session.close();
+		return SellerList;
+	}
 
+    public Seller listBySellerId(Integer sellerId){
+        Session session = this.sessionFactory.openSession();
+        String query= "FROM Seller WHERE id = " + sellerId;
+        Seller seller = (Seller) session.createQuery(query).list().get(0);
+        session.close();
+        return seller;    
+    }
+	
+	@SuppressWarnings("unchecked")
+    public List<Seller> listByApartmentId(Integer apartmentId){
+        Session session = this.sessionFactory.openSession();
+        String query= "FROM Seller WHERE apartment_id = " + apartmentId;
+        List<Seller> seller_by_apartment_id = session.createQuery(query).list();
+        session.close();
+        return seller_by_apartment_id;    
+    }
+	
 }
