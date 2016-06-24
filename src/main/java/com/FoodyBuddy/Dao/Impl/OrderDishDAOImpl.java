deae41392cfadd05dbfe5e408c64151a091ef9fa
junaid ahmed <junzy1994@gmail.com>
2016-06-24 17:41:42 +0530

    Added OrderDishService, sumDishPrice() in orderdishdaoimpl

--- src/main/java/com/FoodyBuddy/Dao/Impl/OrderDishDAOImpl.java
+++ src/main/java/com/FoodyBuddy/Dao/Impl/OrderDishDAOImpl.java
@@ -5,19 +5,17 @@ import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.springframework.stereotype.Repository;
 
 import com.FoodyBuddy.Dao.OrderDishDAO;
-import com.FoodyBuddy.Model.Country;
 import com.FoodyBuddy.Model.OrderDish;
-import com.FoodyBuddy.Model.OrderDish;
-import com.FoodyBuddy.Model.Seller;
 
-
+@Repository
 public class OrderDishDAOImpl implements OrderDishDAO {
 
 	private SessionFactory sessionFactory;
 
-    public void setSessionFactory(SessionFactory sessionFactory) {
+    public OrderDishDAOImpl(SessionFactory sessionFactory) {
         this.sessionFactory = sessionFactory;
     }
     
@@ -46,11 +44,11 @@ public class OrderDishDAOImpl implements OrderDishDAO {
 	}
 	
 	@SuppressWarnings("unchecked")
-	public List<OrderDish> listById(Integer Id) {
+	public OrderDish listById(Integer Id) {
 		Session session = this.sessionFactory.openSession();
 		List<OrderDish> OrderDishList = session.createQuery("from Order_dish").list();
 		session.close();
-		return OrderDishList;
+		return OrderDishList.get(0);
 	}
 
 	@SuppressWarnings("unchecked")
@@ -61,17 +59,19 @@ public class OrderDishDAOImpl implements OrderDishDAO {
 		return OrderDishList;
 	}
 
-	public void update(OrderDish c) {
+	public void  update(OrderDish c) throws Exception{
 		Session session = this.sessionFactory.openSession();
 		try {
 			Transaction tx = session.beginTransaction();
 			session.update(c);
 			tx.commit();
+			session.close();	
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			throw new Exception("Not updated ");
+			
 		}
-		session.close();	
 	}
 
 	public void delete(Integer id) {
@@ -81,6 +81,13 @@ public class OrderDishDAOImpl implements OrderDishDAO {
 			session.delete(OrderDish);
 		}
 		
+	}
+
+	public Integer sumDishPrice(Integer OrderId) {
+		Session session = this.sessionFactory.openSession();
+		int netDishPrice = session.createQuery("SELECT SUM(net_dish_price) FROM order_dish GROUP BY order_id").getFirstResult();
+		session.close();
+		return netDishPrice;
 	}
 
 }
--- src/main/java/com/FoodyBuddy/Dao/OrderDishDAO.java
+++ src/main/java/com/FoodyBuddy/Dao/OrderDishDAO.java
@@ -6,11 +6,12 @@ import com.FoodyBuddy.Model.OrderDish;
 public interface OrderDishDAO {
 
 	public void save(OrderDish s);
-	public void update(OrderDish s);
+	public void update(OrderDish s) throws Exception;
 	public void delete(Integer id);
+	public Integer sumDishPrice(Integer OrderId);
 	public List<OrderDish> listByOrderId(Integer OrderId);
 	public List<OrderDish> listByDishId(Integer DishId);
-	public List<OrderDish> listById(Integer Id);
+	public OrderDish listById(Integer Id);
 	public List<OrderDish> list();
 	
 }
