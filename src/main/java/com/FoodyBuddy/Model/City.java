73dba7302f936672b2229fbfd3a9bf40199a0cab
Tanmay <tanmaykacker40@gmail.com>
2016-06-26 20:26:05 +0530

    Updated models with audit tags
    Fixed issues expressed in Code review.
    
    Classes Covered
    
    - Buyers
    - Country
    - States
    - City
    - Locality
    - Apartments

---
+++ src/main/java/com/FoodyBuddy/Model/City.java
@@ -0,0 +1,97 @@
+package com.FoodyBuddy.Model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import org.hibernate.envers.Audited;
+
+@Entity
+@Audited
+@Table(name = "City")
+public class City {
+	
+	@Id
+	@GeneratedValue
+	@Column(name = "id", nullable = false)
+	private int id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "state_id", nullable = false)
+	@ManyToOne
+	@JoinColumn(name = "id")
+	private State state;
+	
+	public City(String name, State state){
+		this.name = name;
+		this.state = state;
+	}
+	
+	public int getId(){
+		return id;
+	}
+	
+	public void setId(int id){
+		this.id = id;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public State getState(){
+		return state;
+	}
+	
+	public void setState(State state){
+		this.state = state;
+	}
+
+	@Override
+	public String toString() {
+		return "City [id=" + id + ", name=" + name + ", state=" + state + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((state == null) ? 0 : state.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		City other = (City) obj;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (state == null) {
+			if (other.state != null)
+				return false;
+		} else if (!state.equals(other.state))
+			return false;
+		return true;
+	}
+	
+}
--- src/main/java/com/FoodyBuddy/Model/Country.java
+++ src/main/java/com/FoodyBuddy/Model/Country.java
@@ -1,19 +1,20 @@
 package com.FoodyBuddy.Model;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
+import org.hibernate.envers.Audited;
 
 
 @Entity
+@Audited
 @Table(name="Country")
 public class Country{
 	
 	@Id
-	@Column(name="id")
+	@Column(name="id", nullable = false)
 	@GeneratedValue
 	private int id;
 	
@@ -21,6 +22,7 @@ public class Country{
 	private String name;
 
 	public Country(){}
+	
 	public Country(String name){
 		this.name=name;
 	}
@@ -31,6 +33,38 @@ public class Country{
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Country [id=" + id + ", name=" + name + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Country other = (Country) obj;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
 	}
 
 
