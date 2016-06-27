76f896352f8d2b406bfee57bfa65d162ebae7084
sairam <sairamch04@gmail.com>
2016-06-27 11:55:01 +0530

    Update Order.java

--- src/main/java/com/FoodyBuddy/Model/Order.java
+++ src/main/java/com/FoodyBuddy/Model/Order.java
@@ -1,9 +1,10 @@
-package com.FoodyBuddy.Model;
+package com.foodybuddy.model;
 import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -11,41 +12,39 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+import org.hibernate.envers.Audited;
+
 @Entity
-@Table(name="Order")
 @Audited
+@Table(name="orders")
 public class Order{
 	
 	@Id
 	@Column(name="id")
-	@GeneratedValue
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;	
 	
-	@Column(name="name" , nullable = false)
-	private String name;
-	
-	@Column(name = "buyer_id", nullable = false)
     @ManyToOne
-    @JoinColumn(name = "id")
+    @JoinColumn(name = "buyer_id")
     private Buyer  buyer;
 	
-	@Column(name ="status", nullable = false, columnDefinition ="STRING default 'intiated'")
-	private String status;
+	@Column(name ="status", nullable = false, columnDefinition ="int default 1")
+	private int status;
 	
 	@Column(name = "net_order_amount")
 	private int netOrderAmount;
 	
 
 	@Temporal(TemporalType.TIMESTAMP)
-    @Column(name = "created_at", nullable = false)
+    @Column(name = "created_at", columnDefinition="TIMESTAMP default CURRENT_TIMESTAMP")
     private Date createdAt;
 	
 	@Temporal(TemporalType.TIMESTAMP)
-    @Column(name = "updated_at", nullable = false)
+    @Column(name = "updated_at",columnDefinition="TIMESTAMP default CURRENT_TIMESTAMP")
     private Date updatedAt;
 	
 	@Temporal(TemporalType.TIMESTAMP)
-    @Column(name = "deleted_at", nullable = false)
+    @Column(name = "deleted_at",columnDefinition="TIMESTAMP default CURRENT_TIMESTAMP")
     private Date deletedAt;
 
 	public int getId() {
@@ -55,14 +54,6 @@ public class Order{
 	public void setId(int id) {
 		this.id = id;
 	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
 	public Buyer getBuyer() {
 		return buyer;
 	}
@@ -71,11 +62,11 @@ public class Order{
 		this.buyer = buyer;
 	}
 
-	public String getStatus() {
+	public int getStatus() {
 		return status;
 	}
 
-	public void setStatus(String status) {
+	public void setStatus(int status) {
 		this.status = status;
 	}
 
@@ -116,9 +107,8 @@ public class Order{
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		result = prime * result + netOrderAmount;
-		result = prime * result + ((status == null) ? 0 : status.hashCode());
+		result = prime * result + status;
 		return result;
 	}
 
@@ -131,23 +121,15 @@ public class Order{
 		if (getClass() != obj.getClass())
 			return false;
 		Order other = (Order) obj;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
 		if (netOrderAmount != other.netOrderAmount)
 			return false;
-		if (status == null) {
-			if (other.status != null)
-				return false;
-		} else if (!status.equals(other.status))
+		if (status != other.status)
 			return false;
 		return true;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Order [name=" + name + ", status=" + status + ", netOrderAmount=" + netOrderAmount + "]";
+		return "Order [id=" + id +"status=" + status + ", netOrderAmount=" + netOrderAmount + "]";
 	}
 }
