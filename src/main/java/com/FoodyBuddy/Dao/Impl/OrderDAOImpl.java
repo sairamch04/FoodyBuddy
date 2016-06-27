27c6a3fb8a2c6f48fa1b44917fe75a25ee5fba48
sairam <sairamch04@gmail.com>
2016-06-27 12:05:56 +0530

    Update OrderDAOImpl.java

--- src/main/java/com/FoodyBuddy/Dao/Impl/OrderDAOImpl.java
+++ src/main/java/com/FoodyBuddy/Dao/Impl/OrderDAOImpl.java
@@ -1,44 +1,38 @@
-package com.FoodyBuddy.DAO.Impl;
-
+package com.foodybuddy.dao.impl;
 import java.util.List;
 import org.hibernate.Session;
-import org.springframework.stereotype.Repository;
-import com.FoodyBuddy.DAO.OrderDAO;
-import com.FoodyBuddy.Model.Order;
+import com.foodybuddy.dao.DishDAO;
+import com.foodybuddy.model.Dish;
 
-@Repository
-public class OrderDAOImpl implements OrderDAO {
-
+public class DishDAOImpl implements DishDAO {
 	private Session session;
-
-    public OrderDAOImpl(Session session){
-    	 this.session = session;
-    }    
-    public void save(Order order) {
-		session.persist(order);
+	public DishDAOImpl(Session session) {
+        this.session = session;
+    }
+	public void insert(Dish d) {
+		session.persist(d);
 	}
-	
+	public void update(Dish Dish ){
+		     session.update(Dish); 
+	}
+	public void delete(Integer id) {
+		Dish Dish = (Dish) session.load(Dish.class, new Integer(id));
+	}
+
 	@SuppressWarnings("unchecked")
-	public List<Order> getListByBuyerId(Integer buyerId){
-		String query= "FROM Order WHERE buyer_id = " + buyerId;
-		List<Order> orders = session.createQuery(query).list();
-		return orders;		
+	public List<Dish> getListdish() {
+		List<Dish> DishList = session.createQuery("from Dish").list();
+		return DishList;
 	}
-	
+    public Dish getListByDishId(Integer DishId){
+        String query= "FROM Dish WHERE id = " + DishId;
+        Dish dish = (Dish) session.createQuery(query).list().get(0);
+        return dish;    
+    }
 	@SuppressWarnings("unchecked")
-	public Order getById(Integer orderId){
-		String query= "FROM Order WHERE id = " + orderId;
-		Order order = (Order) session.createQuery(query).list().get(0);
-		return order;		
-	}
-	
-	public void update(Order order) {
-		session.update(order);	
-	}
-
-	public void delete(Order  order) {
-		session.delete(order);		
-	}
-
-	
+    public List<Dish> getListBySellerId(Integer sellerid){
+        String query= "FROM Dish WHERE aseller_id = " + sellerid;
+        List<Dish> Dish_by_seller_id = session.createQuery(query).list();
+        return Dish_by_seller_id;    
+    }
 }
