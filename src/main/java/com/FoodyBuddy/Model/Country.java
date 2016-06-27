6f1001b0164fd83e0e3783e035bf5dd32807026d
kanmaytacker <tanmaykacker40@gmail.com>
2016-06-27 12:30:40 +0530

    Fixed issues expressed in Code Review.
    Fixed Issues
    - GenerationType to IDENTITY
    - lowercase package naming

--- src/main/java/com/FoodyBuddy/Model/Country.java
+++ src/main/java/com/FoodyBuddy/Model/Country.java
@@ -1,8 +1,9 @@
-package com.FoodyBuddy.Model;
+package com.foodybuddy.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import org.hibernate.envers.Audited;
@@ -15,7 +16,7 @@ public class Country{
 	
 	@Id
 	@Column(name="id", nullable = false)
-	@GeneratedValue
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	
 	@Column(name="name")
