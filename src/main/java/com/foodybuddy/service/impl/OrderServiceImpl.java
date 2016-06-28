58742292434e8e46ed16675efae7b6acda18a1ac
Sairam <sairamch04@gmail.com>
2016-06-28 19:09:28 +0530

    added OrderService files and its unit test

---
+++ src/main/java/com/foodybuddy/service/impl/OrderServiceImpl.java
@@ -0,0 +1,217 @@
+package com.foodybuddy.service.impl;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.stereotype.Service;
+
+import com.foodybuddy.dao.BuyerDAO;
+import com.foodybuddy.dao.DishDAO;
+import com.foodybuddy.dao.OrderDAO;
+import com.foodybuddy.dao.OrderDishDAO;
+import com.foodybuddy.dao.impl.BuyerDAOImpl;
+import com.foodybuddy.dao.impl.DishDAOImpl;
+import com.foodybuddy.dao.impl.OrderDAOImpl;
+import com.foodybuddy.dao.impl.OrderDishDAOImpl;
+import com.foodybuddy.model.Buyer;
+import com.foodybuddy.model.Dish;
+import com.foodybuddy.model.Order;
+import com.foodybuddy.model.OrderDish;
+import com.foodybuddy.service.OrderService;
+import com.foodybuddy.utils.OrderStatus;
+
+/**
+ * The Class OrderServiceImpl.
+ */
+@Service
+public class OrderServiceImpl implements OrderService {
+
+	/** The session factory. */
+	private SessionFactory sessionFactory;
+	
+	/** The log. */
+	static Logger log = Logger.getLogger(OrderServiceImpl.class.getName());
+
+	/**
+	 * Instantiates a new order service impl.
+	 *
+	 * @param sessionFactory the session factory
+	 */
+	public OrderServiceImpl(SessionFactory sessionFactory) throws NullPointerException{
+		if(sessionFactory == null){
+			throw new NullPointerException("sessionFactory can't be null");
+			
+		}
+		this.sessionFactory = sessionFactory;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.foodybuddy.service.OrderService#placeOrder(int, java.util.List, java.util.List)
+	 */
+	public void placeOrder(int buyerId, List<Integer> dishIds, List<Integer> orderedQuantitys)
+			throws HibernateException {
+		if(buyerId <=0){
+			throw new HibernateException("Invalid buyerId " + buyerId);
+		}
+		Transaction transaction = null;
+		Session session = null;
+		try {
+			session = sessionFactory.openSession();
+			transaction = session.beginTransaction();
+			OrderDAO orderDAO = new OrderDAOImpl(session);
+			BuyerDAO buyerDAO = new BuyerDAOImpl(session);
+			Order order = new Order();
+			order.setStatus(OrderStatus.INTIATED);
+			Buyer buyer = buyerDAO.getById(buyerId);
+
+			for (int index = 0; index < dishIds.size(); index++) {
+				int dishId = dishIds.get(index);
+				int orderedQuantity = orderedQuantitys.get(index);
+				if(dishId <= 0){
+					throw new HibernateException("Invalid buyerId " + buyerId);
+				}
+				if(orderedQuantity <= 0){
+					throw new HibernateException("Invalid buyerId " + buyerId);
+					
+				}
+				// set properties for Order and also for depended models
+				// OrderDish and Dish
+				setPropertiesForOrder(order, buyer, dishId, orderedQuantity, session);
+			}
+			// Inserting order to db
+			orderDAO.insert(order);
+			transaction.commit();
+			log.info("The transaction for update was done succesfully");
+		} catch (Exception ex) {
+
+			try {
+				if (transaction == null) {
+					log.error("Transaction could not be completed" + ex.getMessage());
+					throw new HibernateException("Transaction could not be completed: " + ex.getMessage(), ex);
+				}
+				transaction.rollback();
+				log.error("Transaction could not be completed will be rollbacked: " + ex.getMessage());
+				throw new HibernateException(
+						"Transaction could not be completed will be rollbacked: " + ex.getMessage(), ex);
+			} catch (RuntimeException rbe) {
+				log.error("Transaction could not be completed and rollback failed: " + ex.getMessage());
+				throw new HibernateException(
+						"Transaction could not be completed and rollback failed: " + ex.getMessage(), ex);
+			}
+		} finally {
+			if (session != null) {
+				session.close();
+			}
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see com.foodybuddy.service.OrderService#getOrder(int)
+	 */
+	public Order getOrder(int orderId) throws HibernateException {
+		if(orderId <= 0){
+			throw new HibernateException("Invalid orderId" + orderId);
+		}
+		Session session = null;
+		Order order = null;
+		try {
+			session = sessionFactory.openSession();
+			OrderDAO orderDAO = new OrderDAOImpl(session);
+			order = orderDAO.getById(orderId);
+			log.info("seccesfully obtained object for " + orderId);
+
+		} catch (Exception ex) {
+
+			throw new HibernateException("getOrder failed due to unavialble quanity or invalid orderId",ex);
+			
+		} finally {
+			if (session != null) {
+				session.close();
+			}
+		}
+		return order;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.foodybuddy.service.OrderService#cancelOrder(int)
+	 */
+	// TODO:: check if the action is within availableTill
+	public void cancelOrder(int orderId) throws HibernateException {
+		if(orderId <= 0){
+			throw new HibernateException("Invalid orderId" + orderId);
+		}
+		Transaction transaction = null;
+		Session session = null;
+		try {
+			session = sessionFactory.openSession();
+			transaction = session.beginTransaction();
+			OrderDAO orderDAO = new OrderDAOImpl(session);
+			Order order = orderDAO.getById(orderId);
+			order.setStatus(OrderStatus.CANCELLED);
+			orderDAO.update(order);
+			transaction.commit();
+			log.info("cancel of order was succesfull" + orderId);
+		} catch (Exception ex) {
+
+			try {
+				transaction.rollback();
+				log.error("Transaction could not be completed will be rollbacked: " + ex.getMessage());
+				throw new HibernateException(
+						"Transaction could not be completed will be rollbacked: " + ex.getMessage(), ex);
+			} catch (RuntimeException rbe) {
+				log.error("Transaction could not be completed and rollback failed: " + ex.getMessage());
+				throw new HibernateException(
+						"Transaction could not be completed and rollback failed: " + ex.getMessage(), ex);
+			}
+		} finally {
+			if (session != null) {
+				session.close();
+			}
+		}
+	}
+
+	/**
+	 * Sets the properties for order.
+	 *
+	 * @param order the order
+	 * @param buyer the buyer
+	 * @param dishId the dish id
+	 * @param orderedQuantity the ordered quantity
+	 * @param session the session
+	 * @throws Exception the exception
+	 */
+	private void setPropertiesForOrder(Order order, Buyer buyer, int dishId, int orderedQuantity, Session session)
+			throws Exception {
+		// create the required DAOs
+		DishDAO dishDAO = new DishDAOImpl(session);
+		OrderDishDAO orderDishDAO = new OrderDishDAOImpl(session);
+
+		Dish dish = dishDAO.getListByDishId(dishId);
+		if (dish == null) {
+			log.info(dishId + " is not valid ");
+			throw new Exception("Invalid dishId : " + dishId);
+		}
+		// TODO:: check if ordered times is before AvialaleTill
+		if (dish.getQuantityAvailable() >= orderedQuantity) {
+			OrderDish orderDish = new OrderDish();
+			orderDish.setOrder(order);
+			orderDish.setDish(dish);
+			dish.setQuantityAvailable(dish.getQuantityAvailable() - orderedQuantity);
+			orderDish.setQuantity(orderedQuantity);
+			orderDish.setNetDishPrice(orderedQuantity * dish.getPrice());
+			order.setBuyer(buyer);
+
+			orderDishDAO.insert(orderDish);
+			order.setNetOrderAmount(order.getNetOrderAmount() + orderDish.getNetDishPrice());
+		} else {
+			log.info("Quantity not available");
+			throw new Exception("Quantity not available");
+		}
+
+	}
+
+}
---
+++ src/test/java/com/foodybuddy/service/OrderServiceTest.java
@@ -0,0 +1,418 @@
+package com.foodybuddy.service;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.foodybuddy.dao.BuyerDAO;
+import com.foodybuddy.dao.DishDAO;
+import com.foodybuddy.dao.impl.BuyerDAOImpl;
+import com.foodybuddy.dao.impl.DishDAOImpl;
+import com.foodybuddy.model.Buyer;
+import com.foodybuddy.model.Dish;
+import com.foodybuddy.model.Order;
+import com.foodybuddy.service.OrderService;
+import com.foodybuddy.service.impl.OrderServiceImpl;
+import com.foodybuddy.utils.OrderStatus;
+import com.foodybuddy.utils.SessionFactoryUtils;
+
+import junit.framework.TestCase;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class OrderServiceTest.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/hibernate.cfg.xml")
+public class OrderServiceTest extends TestCase {
+
+	/** The log. */
+	static Log log = LogFactory.getLog(OrderServiceTest.class.getName());
+	
+	/** The buyer. */
+	Buyer buyer = null;
+	
+	/** The session factory. */
+	SessionFactory sessionFactory = null;
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	@Before
+	@Override
+	public void setUp() throws Exception {
+		if(sessionFactory == null)
+			sessionFactory = SessionFactoryUtils.getSessionFactory();
+		insertDependentData();	
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	@After
+	@Override
+	public void tearDown() throws Exception {
+		//flush the inserted table
+		//cleanUpDependentData();	
+	}
+
+	/**
+	 * Test for placeOrder.
+	 */
+	@Test
+	public void placeOrderTest() {
+		try {
+			OrderService orderService = new OrderServiceImpl(sessionFactory);
+			// set parameters
+			int buyerId = 1;
+			int dishId = 1;
+			int orderedQuantity = 2;
+
+			// prepare the arguments to be passed
+			List<Integer> dishIds = new ArrayList<Integer>();
+			dishIds.add(dishId);
+			List<Integer> orderedQuantitys = new ArrayList<Integer>();
+			orderedQuantitys.add(orderedQuantity);
+
+			// place the order
+			orderService.placeOrder(buyerId, dishIds, orderedQuantitys);
+
+			// Fetch the inserted order
+			int orderId = 1;
+			Order order = orderService.getOrder(orderId);
+
+			// Test the fetched object
+			assertNotNull(order);
+			assertNotNull(order.getBuyer());
+			assertEquals(order.getNetOrderAmount(), 200); // 2 biryani's cost = 2*100															
+			assertEquals(order.getBuyer().getId(), buyerId);
+			assertEquals(order.getBuyer().getName(), "FoodyBuddy");
+			assertEquals(order.getStatus(), OrderStatus.INTIATED);
+		} catch (Exception ex) {
+			assertTrue(false);
+			System.out.println(ex);
+			log.error(ex);
+		}
+	}
+	/**
+	 * Negative test
+	 * Placing the order for quantity more than available
+	 */
+	@Test(expected = Exception.class)
+	public void placeInvalidOrderTest() throws Exception{
+		try {
+			OrderService orderService = new OrderServiceImpl(sessionFactory);
+			// set parameters
+			int buyerId = 1;
+			int dishId = 1;
+			int orderedQuantity = 20;
+
+			// prepare the arguments to be passed
+			List<Integer> dishIds = new ArrayList<Integer>();
+			dishIds.add(dishId);
+			List<Integer> orderedQuantitys = new ArrayList<Integer>();
+			orderedQuantitys.add(orderedQuantity);
+
+			// place the order
+			orderService.placeOrder(buyerId, dishIds, orderedQuantitys);
+		} catch (Exception ex) {
+			log.error(ex);
+			throw ex;
+		}
+	}
+	
+	/**
+	 * Check by passing negative value of orderedQuanity.
+	 *
+	 * @throws Exception the exception
+	 */	
+	@Test(expected = Exception.class)
+	public void negativePlaceOrderTest() throws Exception{
+		OrderService orderService = new OrderServiceImpl(sessionFactory);
+		// set parameters
+		int buyerId = 1;
+		int dishId = 1;
+		int orderedQuantity = -2;
+
+		// prepare the arguments to be passed
+		List<Integer> dishIds = new ArrayList<Integer>();
+		dishIds.add(dishId);
+		List<Integer> orderedQuantitys = new ArrayList<Integer>();
+		orderedQuantitys.add(orderedQuantity);
+
+		// place the order
+		orderService.placeOrder(buyerId, dishIds, orderedQuantitys);
+			
+		
+	}
+
+	/**
+	 * Test for canceling the order, basically setting the status flag to CANCELLED.
+	 */
+	@Test
+	public void cancelOrderTest() {
+		try {
+			OrderService orderService = new OrderServiceImpl(SessionFactoryUtils.getSessionFactory());
+			// set parameters
+			int orderId = 1;
+
+			// cancel the order
+			orderService.cancelOrder(orderId);
+
+			// Fetch the cancelled order
+			Order order = orderService.getOrder(orderId);
+			// Test the fetched object
+			assertNotNull(order);
+			assertEquals(order.getStatus(), OrderStatus.CANCELLED);
+
+		} catch (Exception ex) {
+			assertTrue(false);
+			System.out.println(ex);
+			log.error(ex);
+		}
+
+	}
+	
+	/**
+	 * Negative test
+	 * Check the action on passing sessionFactory as null.
+	 *
+	 * @throws Exception the exception
+	 */
+	@Test(expected = Exception.class)
+	public void negativeServieObjectTest() throws Exception{
+		try {
+			OrderService orderService = new OrderServiceImpl(null);
+		} catch (Exception ex) {
+			System.out.println(ex);
+			log.error(ex);
+			throw ex;
+		}
+
+	}
+	
+	/**
+	 * Negative test
+	 * We check with the orderId which is not available in the database.
+	 *
+	 * @throws Exception the exception
+	 */
+	@Test(expected = Exception.class)
+	public void negativeInvalidCancelOrderTest() throws Exception {
+		try {
+			OrderService orderService = new OrderServiceImpl(SessionFactoryUtils.getSessionFactory());
+			// set parameters
+			int orderId = 100;
+
+			// cancel the order
+			orderService.cancelOrder(orderId);
+
+			// Fetch the cancelled order
+			Order order = orderService.getOrder(orderId);
+			System.out.println(order);
+
+		} catch (Exception ex) {
+			System.out.println(ex);
+			log.error(ex);
+			throw ex;
+		}
+
+	}
+	
+	/**
+	 * Negative test
+	 * We check with the orderId as negative.
+	 *
+	 * @throws Exception the exception
+	 */
+	@Test(expected = Exception.class)
+	public void negativeCancelOrderTest() throws Exception {
+		try {
+			OrderService orderService = new OrderServiceImpl(SessionFactoryUtils.getSessionFactory());
+			// set parameters
+			int orderId = -1;
+
+			// cancel the order
+			orderService.cancelOrder(orderId);
+
+			// Fetch the cancelled order
+			Order order = orderService.getOrder(orderId);
+
+		} catch (Exception ex) {
+			System.out.println(ex);
+			log.error(ex);
+			throw ex;
+		}
+
+	}
+
+	/**
+	 * Test for getting the order object by orderId.
+	 *
+	 * @return the order test
+	 */
+	@Test
+	public void getOrderTest() {
+		try {
+			OrderService orderService = new OrderServiceImpl(SessionFactoryUtils.getSessionFactory());
+			// set parameters
+			int orderId = 1;
+			// get the order
+			orderService.getOrder(orderId);
+
+			// Fetch the cancelled order
+			Order order = orderService.getOrder(orderId);
+
+			// Test the fetched object
+			assertNotNull(order);
+			assertEquals(order.getStatus(), OrderStatus.INTIATED);
+			assertNotNull(order.getBuyer());
+			assertEquals(order.getBuyer().getId(), 1);
+
+		} catch (Exception ex) {
+			assertTrue(false);
+			System.out.println(ex);
+			log.error(ex);
+		}
+
+	}
+	
+	/**
+	 * Negative test
+	 * Pass the negative orderId.
+	 *
+	 * @throws Exception the exception
+	 */
+	
+	@Test(expected = Exception.class)
+	public void negativeGetOrderTest() throws Exception{
+		try{
+			OrderService orderService = new OrderServiceImpl(SessionFactoryUtils.getSessionFactory());
+			// set parameters
+			int orderId = -11;
+			// get the order
+			orderService.getOrder(orderId);	
+			// Fetch the cancelled order
+			Order order = orderService.getOrder(orderId);
+		}
+		catch(Exception ex){
+			throw ex;		
+		}		
+	}
+	
+	/**
+	 * Negative test
+	 * Pass the  orderId which is not available in database.
+	 *
+	 * @throws Exception the exception
+	 */
+	
+	@Test(expected = Exception.class)
+	public void negativeInvalidGetOrderTest() throws Exception{
+		try{
+			OrderService orderService = new OrderServiceImpl(SessionFactoryUtils.getSessionFactory());
+			// set parameters
+			int orderId = 111;
+			// get the order
+			orderService.getOrder(orderId);	
+			// Fetch the cancelled order
+			Order order = orderService.getOrder(orderId);
+		}
+		catch(Exception ex){
+			throw ex;		
+		}		
+	}
+
+	/**
+	 * Pre-processing
+	 * This is required for inserting buyer, dish into the database because
+	 * placeOrder depends on them.
+	 *
+	 * @throws Exception the exception
+	 */
+	private void insertDependentData()  throws Exception{
+		Session session = null;
+		try {
+			
+			if(sessionFactory == null){
+				throw new NullPointerException("session can't be null");
+			}
+			session = sessionFactory.openSession();
+			session.beginTransaction();
+			//insert buyer
+			buyer = new Buyer();
+			buyer.setEmail("sairamch04@gmail.com");
+			buyer.setName("FoodyBuddy");
+			buyer.setPhoneNumber("9908219617");
+			buyer.setFlatNumber("300 B");
+			BuyerDAO buyerDAO= new BuyerDAOImpl(session);
+			buyerDAO.insert(buyer);
+			//insert Dish
+			Dish dish = new Dish();
+			dish.setDescription("little sweet");
+			dish.setIsVeg(true);
+			dish.setName("Biryani");
+			dish.setPrice(100);
+			dish.setQuantityAvailable(10);
+			DishDAO dishDAO = new DishDAOImpl(session);
+			dishDAO.insert(dish);
+			session.getTransaction().commit();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			log.error(ex);
+			throw ex;
+		}
+		finally{
+			if(session!=null)
+				session.close();
+			
+		}
+	}
+	/**
+	 * Post-processing code : for cleaning up database tables
+	 */
+//	private void cleanUpDependentData() throws Exception{
+//		Session session = null;
+//		try {
+//
+//			if(sessionFactory == null){
+//				throw new NullPointerException("session can't be null");
+//			}
+//			session = sessionFactory.openSession();
+//			session.beginTransaction();
+//			//delete buyer
+//			BuyerDAO buyerDAO= new BuyerDAOImpl(session);
+//			Buyer buyer = buyerDAO.getById(1);
+//			buyerDAO.delete(buyer);
+//			//delete Dish
+//			DishDAO dishDAO = new DishDAOImpl(session);
+//			Dish dish = dishDAO.getListByDishId(1);
+//			dishDAO.delete(dish);
+//			session.getTransaction().commit();
+//		} catch (Exception ex) {
+//			ex.printStackTrace();
+//			log.error(ex);
+//			throw ex;
+//		}
+//		finally{
+//			if(session!=null)
+//				session.close();
+//			
+//		}
+//		
+//	}
+}
