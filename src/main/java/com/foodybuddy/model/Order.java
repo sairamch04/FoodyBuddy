1dc37c6f64db4c22f1ddc45b98ecb193c8dd6f56
Sairam <sairamch04@gmail.com>
2016-06-28 20:47:49 +0530

    updated insert method to return object

--- src/main/java/com/foodybuddy/model/Order.java
+++ src/main/java/com/foodybuddy/model/Order.java
@@ -1,5 +1,4 @@
 package com.foodybuddy.model;
-
 import java.util.Date;
 
 import javax.persistence.Column;
@@ -15,186 +14,95 @@ import javax.persistence.TemporalType;
 
 import org.hibernate.envers.Audited;
 
-/**
- * The Class Order.
- */
 @Entity
 @Audited
-@Table(name = "orders")
-public class Order {
-
-	/** The id. */
+@Table(name="orders")
+public class Order{
+	
 	@Id
-	@Column(name = "id")
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
-
-	/** The buyer. */
-	@ManyToOne
-	@JoinColumn(name = "buyer_id", nullable = false)
-	private Buyer buyer;
-
-	/** The status. */
-	@Column(name = "status", nullable = false)
+	@Column(name="id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;	
+	
+    @ManyToOne
+    @JoinColumn(name = "buyer_id")
+    private Buyer  buyer;
+	
+	@Column(name ="status", nullable = false, columnDefinition ="int default 1")
 	private int status;
-
-	/** The net order amount. */
-	@Column(name = "net_order_amount", nullable = false)
+	
+	@Column(name = "net_order_amount")
 	private int netOrderAmount;
+	
 
-	/** The created at. */
 	@Temporal(TemporalType.TIMESTAMP)
-	@Column(name = "created_at")
-	private Date createdAt;
-
-	/** The updated at. */
+    @Column(name = "created_at", columnDefinition="TIMESTAMP default CURRENT_TIMESTAMP")
+    private Date createdAt;
+	
 	@Temporal(TemporalType.TIMESTAMP)
-	@Column(name = "updated_at")
-	private Date updatedAt;
-
-	/** The deleted at. */
+    @Column(name = "updated_at",columnDefinition="TIMESTAMP default CURRENT_TIMESTAMP")
+    private Date updatedAt;
+	
 	@Temporal(TemporalType.TIMESTAMP)
-	@Column(name = "deleted_at")
-	private Date deletedAt;
+    @Column(name = "deleted_at",columnDefinition="TIMESTAMP default CURRENT_TIMESTAMP")
+    private Date deletedAt;
 
-	/**
-	 * Gets the id.
-	 *
-	 * @return the id
-	 */
 	public int getId() {
 		return id;
 	}
 
-	/**
-	 * Sets the id.
-	 *
-	 * @param id
-	 *            the new id
-	 */
 	public void setId(int id) {
 		this.id = id;
 	}
-
-	/**
-	 * Gets the buyer.
-	 *
-	 * @return the buyer
-	 */
 	public Buyer getBuyer() {
 		return buyer;
 	}
 
-	/**
-	 * Sets the buyer.
-	 *
-	 * @param buyer
-	 *            the new buyer
-	 */
 	public void setBuyer(Buyer buyer) {
 		this.buyer = buyer;
 	}
 
-	/**
-	 * Gets the status.
-	 *
-	 * @return the status
-	 */
 	public int getStatus() {
 		return status;
 	}
 
-	/**
-	 * Sets the status.
-	 *
-	 * @param status
-	 *            the new status
-	 */
 	public void setStatus(int status) {
 		this.status = status;
 	}
 
-	/**
-	 * Gets the net order amount.
-	 *
-	 * @return the net order amount
-	 */
 	public int getNetOrderAmount() {
 		return netOrderAmount;
 	}
-
-	/**
-	 * Sets the net order amount.
-	 *
-	 * @param netOrderAmount
-	 *            the new net order amount
-	 */
+	
 	public void setNetOrderAmount(int netOrderAmount) {
 		this.netOrderAmount = netOrderAmount;
 	}
 
-	/**
-	 * Gets the created at.
-	 *
-	 * @return the created at
-	 */
 	public Date getCreatedAt() {
 		return createdAt;
 	}
 
-	/**
-	 * Sets the created at.
-	 *
-	 * @param createdAt
-	 *            the new created at
-	 */
 	public void setCreatedAt(Date createdAt) {
 		this.createdAt = createdAt;
 	}
 
-	/**
-	 * Gets the updated at.
-	 *
-	 * @return the updated at
-	 */
+	
 	public Date getUpdatedAt() {
 		return updatedAt;
 	}
 
-	/**
-	 * Sets the updated at.
-	 *
-	 * @param updatedAt
-	 *            the new updated at
-	 */
 	public void setUpdatedAt(Date updatedAt) {
 		this.updatedAt = updatedAt;
 	}
 
-	/**
-	 * Gets the deleted at.
-	 *
-	 * @return the deleted at
-	 */
 	public Date getDeletedAt() {
 		return deletedAt;
 	}
 
-	/**
-	 * Sets the deleted at.
-	 *
-	 * @param deletedAt
-	 *            the new deleted at
-	 */
 	public void setDeletedAt(Date deletedAt) {
 		this.deletedAt = deletedAt;
 	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Object#hashCode()
-	 */
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -204,11 +112,6 @@ public class Order {
 		return result;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -225,13 +128,8 @@ public class Order {
 		return true;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Object#toString()
-	 */
 	@Override
 	public String toString() {
-		return "Order [id=" + id + "status=" + status + ", netOrderAmount=" + netOrderAmount + "]";
+		return "Order [id=" + id +"status=" + status + ", netOrderAmount=" + netOrderAmount + "]";
 	}
 }
