128f5b2ed7311f6612dce2abd5773b51573844bf
Tanmay <tanmaykacker40@gmail.com>
2016-06-28 18:50:54 +0530

    Updated Models
    Classes
    - Country
    - State
    - City

--- src/main/java/com/foodybuddy/model/State.java
+++ src/main/java/com/foodybuddy/model/State.java
@@ -1,5 +1,6 @@
 package com.foodybuddy.model;
 
+import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,58 +9,192 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
 import org.hibernate.envers.Audited;
 
+
+/**
+ * The Class State.
+ */
 @Entity
 @Audited
 @Table(name = "State")
 public class State {
 	
+	/** The id. */
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name = "id", nullable = false)
-	private int id;
+	private Integer id;
 	
-	@Column(name = "name")
+	/** The name. */
+	@Column(name = "name", nullable = false)
 	private String name;
 	
+	/** The country. */
+	@NotNull
 	@ManyToOne
 	@JoinColumn(name = "country_id")
 	private Country country;
 	
-	public State(String name, Country country){
-		this.name = name;
-		this.country = country;
-	}
+	/** The created at. */
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "created_at")
+	private Date createdAt;
+	
+	/** The modified at. */
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "modified_at")
+	private Date modifiedAt;
+	
+	/** The deleted at. */
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "deleted_at")
+	private Date deletedAt;
+	
+	/** The modified by id. */
+	@Column(name = "modified_by_id")
+	private Integer modifiedById;
 
+	/**
+	 * Gets the id.
+	 *
+	 * @return the id
+	 */
 	public int getId(){
 		return id;
 	}
 	
-	public void setId(int id){
+	/**
+	 * Sets the id.
+	 *
+	 * @param id the new id
+	 */
+	public void setId(Integer id){
 		this.id = id;
 	}
 	
+	/**
+	 * Gets the name.
+	 *
+	 * @return the name
+	 */
 	public String getName(){
 		return name;
 	}
 	
+	/**
+	 * Sets the name.
+	 *
+	 * @param name the new name
+	 */
 	public void setName(String name){
 		this.name = name;
 	}
 	
+	/**
+	 * Gets the country.
+	 *
+	 * @return the country
+	 */
 	public Country getCountry(){
 		return country;
 	}
 	
+	/**
+	 * Sets the country.
+	 *
+	 * @param country the new country
+	 */
 	public void setCountry(Country country){
 		this.country = country;
 	}
 
+	/**
+	 * Gets the created at.
+	 *
+	 * @return the created at
+	 */
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	/**
+	 * Sets the created at.
+	 *
+	 * @param createdAt the new created at
+	 */
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	/**
+	 * Gets the modified at.
+	 *
+	 * @return the modified at
+	 */
+	public Date getModifiedAt() {
+		return modifiedAt;
+	}
+
+	/**
+	 * Sets the modified at.
+	 *
+	 * @param modifiedAt the new modified at
+	 */
+	public void setModifiedAt(Date modifiedAt) {
+		this.modifiedAt = modifiedAt;
+	}
+
+	/**
+	 * Gets the deleted at.
+	 *
+	 * @return the deleted at
+	 */
+	public Date getDeletedAt() {
+		return deletedAt;
+	}
+
+	/**
+	 * Sets the deleted at.
+	 *
+	 * @param deletedAt the new deleted at
+	 */
+	public void setDeletedAt(Date deletedAt) {
+		this.deletedAt = deletedAt;
+	}
+
+	/**
+	 * Gets the modified by id.
+	 *
+	 * @return the modified by id
+	 */
+	public Integer getModifiedById() {
+		return modifiedById;
+	}
+
+	/**
+	 * Sets the modified by id.
+	 *
+	 * @param modifiedById the new modified by id
+	 */
+	public void setModifiedById(Integer modifiedById) {
+		this.modifiedById = modifiedById;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
 	@Override
 	public String toString() {
 		return "State [id=" + id + ", name=" + name + ", country=" + country + "]";
 	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -69,6 +204,9 @@ public class State {
 		return result;
 	}
 	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
