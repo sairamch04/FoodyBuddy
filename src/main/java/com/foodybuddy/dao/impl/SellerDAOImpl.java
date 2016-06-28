5db59e20bcc08dfb916856860ec4a492a1f86f0f
sanjana <sanjanagoyal21@gmail.com>
2016-06-28 21:12:28 +0530

    fixed issues in code review

--- src/main/java/com/foodybuddy/dao/impl/SellerDAOImpl.java
+++ src/main/java/com/foodybuddy/dao/impl/SellerDAOImpl.java
@@ -1,41 +1,84 @@
 package com.foodybuddy.dao.impl;
+
 import java.util.List;
 import org.hibernate.Session;
+import org.springframework.stereotype.Repository;
+
 import com.foodybuddy.dao.SellerDAO;
 import com.foodybuddy.model.Seller;
 
-
+// TODO: Auto-generated Javadoc
+/**
+ * The Class SellerDAOImpl.
+ */
+@Repository
 public class SellerDAOImpl implements SellerDAO {
+	
+	/** The session. */
 	private Session session;
 
-    public SellerDAOImpl(Session session) {
-        this.session = session;
-    }
-	public void insert(Seller s) {
+	/**
+	 * Instantiates a new seller DAO impl.
+	 *
+	 * @param session the session
+	 * @throws NullPointerException the null pointer exception
+	 */
+	public SellerDAOImpl(Session session) throws NullPointerException {
+		if (session == null) {
+			throw new NullPointerException("Session is null");
+		}
+		this.session = session;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.foodybuddy.dao.SellerDAO#insert(com.foodybuddy.model.Seller)
+	 */
+	public Seller insert(Seller s) {
 		session.persist(s);
+		return s;
 	}
-	public void update(Seller seller ){
-		   session.update(seller); 
+ 
+	/* (non-Javadoc)
+	 * @see com.foodybuddy.dao.SellerDAO#update(com.foodybuddy.model.Seller)
+	 */
+	public Seller update(Seller seller) {
+		session.update(seller);
+		return seller;
 	}
+ 
+	/* (non-Javadoc)
+	 * @see com.foodybuddy.dao.SellerDAO#delete(java.lang.Integer)
+	 */
 	public void delete(Integer id) {
-		Seller seller = (Seller) session.load(Seller.class, new Integer(id));
-		session.delete(seller);
+		session.createQuery("delete from Seller where id="+id);
 	}
+
+	/* (non-Javadoc)
+	 * @see com.foodybuddy.dao.SellerDAO#getListSeller()
+	 */
 	@SuppressWarnings("unchecked")
-	public List<Seller> getListSeller() {
-		List<Seller> SellerList = session.createQuery("FROM Seller").list();
+	public List<Seller> getSellerList() {
+		List<Seller> SellerList = session.createQuery("from Seller").list();
 		return SellerList;
 	}
-    public Seller getListBySellerId(Integer sellerId){
-        String query= "FROM Seller WHERE id = " + sellerId;
-        Seller seller = (Seller) session.createQuery(query).list().get(0);
-        return seller;    
-    }
+
+	/* (non-Javadoc)
+	 * @see com.foodybuddy.dao.SellerDAO#getListBySellerId(java.lang.Integer)
+	 */
+	public Seller getBySellerId(Integer sellerId) {
+		String query = "FROM Seller WHERE id = " + sellerId;
+		Seller seller = (Seller) session.createQuery(query).uniqueResult();
+		return seller;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.foodybuddy.dao.SellerDAO#getListByApartmentId(java.lang.Integer)
+	 */
 	@SuppressWarnings("unchecked")
-    public List<Seller> getListByApartmentId(Integer apartmentId){
-        String query= "FROM Seller WHERE apartment_id = " + apartmentId;
-        List<Seller> sellerList = session.createQuery(query).list();
-        return sellerList;    
-    }
-	
+	public List<Seller> getListByApartmentId(Integer apartmentId) {
+		String query = "FROM Seller WHERE apartment_id = " + apartmentId;
+		List<Seller> sellerList = session.createQuery(query).list();
+		return sellerList;
+	}
+
 }
--- src/main/java/com/foodybuddy/model/Seller.java
+++ src/main/java/com/foodybuddy/model/Seller.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -12,141 +13,268 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import org.hibernate.envers.Audited;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Seller.
+ */
 @Entity
 @Audited
 @Table(name="Seller")
 public class Seller{
 	
+	/** The id. */
 	@Id
 	@Column(name="id")
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private int id;
+	private Integer id;
 	
-	@Column(name="name")
+	/** The name. */
+	@Column(name="name" )
 	private String name;
 
-	@Column(name="email")
+	/** The email. */
+	@Column(name="email", nullable=false)
 	private String email;
 
-	@Column(name="mobile")
+	/** The mobile. */
+	@Column(name="mobile", nullable=false)
 	private String mobile;
 
-	@Column(name="flat_no")
+	/** The flat no. */
+	@Column(name="flat_no", nullable=false)
 	private String flatNo;
 	
-	@Column(name = "apartment_id", nullable = false)
+    /** The apartment. */
     @ManyToOne
-    @JoinColumn(name = "id")
+    @JoinColumn(name = "apartment_id")
     private Apartment apartment;
 	
-	@Column(name="is_active" , nullable = false , columnDefinition = "boolean default true")
+	/** The is active. */
+	@Column(name="is_active")
 	private Boolean isActive;
 
+	/** The created at. */
 	@Temporal(TemporalType.TIMESTAMP)
-    @Column(name = "created_at", columnDefinition="TIMESTAMP default CURRENT_TIMESTAMP")
+    @Column(name = "created_at")
     private Date createdAt;
 	
+	/** The updated at. */
 	@Temporal(TemporalType.TIMESTAMP)
-    @Column(name = "updated_at", columnDefinition="TIMESTAMP default CURRENT_TIMESTAMP")
+    @Column(name = "updated_at")
     private Date updatedAt;
 	
+	/** The deleted at. */
 	@Temporal(TemporalType.TIMESTAMP)
-	 @Column(name = "deleted_at", columnDefinition="TIMESTAMP default CURRENT_TIMESTAMP")
+	 @Column(name = "deleted_at")
     private Date deletedAt;
 	
-	public int getId() {
+	/**
+	 * Gets the id.
+	 *
+	 * @return the id
+	 */
+	public Integer getId() {
 		return id;
 	}
 
+	/**
+	 * Gets the name.
+	 *
+	 * @return the name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Sets the name.
+	 *
+	 * @param name the new name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * Gets the email.
+	 *
+	 * @return the email
+	 */
 	public String getEmail() {
 		return email;
 	}
 
+	/**
+	 * Sets the email.
+	 *
+	 * @param email the new email
+	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
+	/**
+	 * Gets the mobile.
+	 *
+	 * @return the mobile
+	 */
 	public String getMobile() {
 		return mobile;
 	}
 
+	/**
+	 * Sets the mobile.
+	 *
+	 * @param mobile the new mobile
+	 */
 	public void setMobile(String mobile) {
 		this.mobile = mobile;
 	}
 
+	/**
+	 * Gets the flat no.
+	 *
+	 * @return the flat no
+	 */
 	public String getFlatNo() {
 		return flatNo;
 	}
 
+	/**
+	 * Sets the flat no.
+	 *
+	 * @param flatNo the new flat no
+	 */
 	public void setFlatNo(String flatNo) {
 		this.flatNo = flatNo;
 	}
 
 
+	/**
+	 * Gets the apartment.
+	 *
+	 * @return the apartment
+	 */
 	public Apartment getApartment() {
 		return apartment;
 	}
 
+	/**
+	 * Sets the apartment.
+	 *
+	 * @param apartment the new apartment
+	 */
 	public void setApartment(Apartment apartment) {
 		this.apartment = apartment;
 	}
 
+	/**
+	 * Gets the created at.
+	 *
+	 * @return the created at
+	 */
 	public Date getCreatedAt() {
 		return createdAt;
 	}
 
+	/**
+	 * Sets the created at.
+	 *
+	 * @param createdAt the new created at
+	 */
 	public void setCreatedAt(Date createdAt) {
 		this.createdAt = createdAt;
 	}
 
+	/**
+	 * Gets the updated at.
+	 *
+	 * @return the updated at
+	 */
 	public Date getUpdatedAt() {
 		return updatedAt;
 	}
 
+	/**
+	 * Sets the updated at.
+	 *
+	 * @param updatedAt the new updated at
+	 */
 	public void setUpdatedAt(Date updatedAt) {
 		this.updatedAt = updatedAt;
 	}
 
+	/**
+	 * Gets the deleted at.
+	 *
+	 * @return the deleted at
+	 */
 	public Date getDeletedAt() {
 		return deletedAt;
 	}
 
+	/**
+	 * Sets the deleted at.
+	 *
+	 * @param deletedAt the new deleted at
+	 */
 	public void setDeletedAt(Date deletedAt) {
 		this.deletedAt = deletedAt;
 	}
 
+	/**
+	 * Gets the checks if is active.
+	 *
+	 * @return the checks if is active
+	 */
 	public Boolean getIsActive() {
 		return isActive;
 	}
 
+	/**
+	 * Sets the checks if is active.
+	 *
+	 * @param isActive the new checks if is active
+	 */
 	public void setIsActive(Boolean isActive) {
 		this.isActive = isActive;
 	}
-	
-	public Seller(){
-	}
-	
-	public Seller(String name, String email, String mobile, String flatNo, Apartment apartment) {
+
+	/**
+	 * Instantiates a new seller.
+	 */
+	public Seller()
+	{}
+
+	/**
+	 * Instantiates a new seller.
+	 *
+	 * @param name the name
+	 * @param email the email
+	 * @param mobile the mobile
+	 * @param flatNo the flat no
+	 * @param apartment the apartment
+	 * @param isActive the is active
+	 */
+	public Seller(String name, String email, String mobile, String flatNo, Apartment apartment, Boolean isActive) {
 		super();
 		this.name = name;
 		this.email = email;
 		this.mobile = mobile;
 		this.flatNo = flatNo;
 		this.apartment = apartment;
+		this.isActive = isActive;
 	}
-	
+
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((apartment == null) ? 0 : apartment.hashCode());
 		result = prime * result + ((email == null) ? 0 : email.hashCode());
 		result = prime * result + ((flatNo == null) ? 0 : flatNo.hashCode());
 		result = prime * result + ((isActive == null) ? 0 : isActive.hashCode());
@@ -155,6 +283,9 @@ public class Seller{
 		return result;
 	}
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -164,6 +295,11 @@ public class Seller{
 		if (getClass() != obj.getClass())
 			return false;
 		Seller other = (Seller) obj;
+		if (apartment == null) {
+			if (other.apartment != null)
+				return false;
+		} else if (!apartment.equals(other.apartment))
+			return false;
 		if (email == null) {
 			if (other.email != null)
 				return false;
@@ -192,6 +328,9 @@ public class Seller{
 		return true;
 	}
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
 	@Override
 	public String toString() {
 		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", mobile=" + mobile + ", flatNo=" + flatNo
