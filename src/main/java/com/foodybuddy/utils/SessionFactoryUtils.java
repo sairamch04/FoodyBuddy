af217f68a5f656fd915ce930de4dac6597a2e377
Sairam <sairamch04@gmail.com>
2016-06-28 10:28:11 +0530

    added lazy intialization for session factory

--- src/main/java/com/foodybuddy/utils/SessionFactoryUtils.java
+++ src/main/java/com/foodybuddy/utils/SessionFactoryUtils.java
@@ -1,4 +1,5 @@
-package com.foodybuddy.utils; 
+package com.foodybuddy.utils;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.hibernate.SessionFactory;
@@ -8,29 +9,27 @@ import org.hibernate.service.ServiceRegistry;
 
 public class SessionFactoryUtils {
 	static Log log = LogFactory.getLog(SessionFactoryUtils.class.getName());
-	private static SessionFactory sessionFactory = null;
+	private static SessionFactory sessionFactory = buildSessionFactory();
+	private SessionFactoryUtils(){}
 
 	private static SessionFactory buildSessionFactory() {
 		try {
-			Configuration configuration = new Configuration()
-					.configure(SessionFactoryUtils.class.getResource("/hibernate.cfg.xml"));
-			StandardServiceRegistryBuilder serviceRegistryBuilder = new StandardServiceRegistryBuilder();
-			serviceRegistryBuilder.applySettings(configuration.getProperties());
-			ServiceRegistry serviceRegistry = serviceRegistryBuilder.build();
-			sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+			if (sessionFactory == null) {
+				Configuration configuration = new Configuration()
+						.configure(SessionFactoryUtils.class.getResource("/hibernate.cfg.xml"));
+				StandardServiceRegistryBuilder serviceRegistryBuilder = new StandardServiceRegistryBuilder();
+				serviceRegistryBuilder.applySettings(configuration.getProperties());
+				ServiceRegistry serviceRegistry = serviceRegistryBuilder.build();
+				sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+			}
 			return sessionFactory;
 		} catch (Throwable ex) {
-			log.error(ex);
-			// Make sure you log the exception, as it might be swallowed
-			System.err.println("Initial SessionFactory creation failed." + ex);
+			log.error("Initial SessionFactory creation failed." + ex);
 			throw new ExceptionInInitializerError(ex);
 		}
 	}
 
 	public static SessionFactory getSessionFactory() {
-		if (sessionFactory == null) {
-			sessionFactory = buildSessionFactory();
-		}
 		return sessionFactory;
 	}
 }
