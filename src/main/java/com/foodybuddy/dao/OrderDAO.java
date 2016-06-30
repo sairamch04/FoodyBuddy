58bed02ddc95b26a6c16fee26110c68b4e3351bf
Sairam <sairamch04@gmail.com>
2016-06-30 15:33:51 +0530

    Added javadoc and fixed issues in code review

--- src/main/java/com/foodybuddy/dao/OrderDAO.java
+++ src/main/java/com/foodybuddy/dao/OrderDAO.java
@@ -4,12 +4,47 @@ import java.util.List;
 
 import com.foodybuddy.model.Order;
 
+/**
+ * The Interface OrderDAO.
+ */
 public interface OrderDAO {
-	
-	public void insert(Order c);
+
+	/**
+	 * Insert.
+	 *
+	 * @param c the c
+	 * @return 
+	 */
+	public Order insert(Order c);
+
+	/**
+	 * Gets the list by buyer id.
+	 *
+	 * @param buyerId the buyer id
+	 * @return the list by buyer id
+	 */
 	public List<Order> getListByBuyerId(int buyerId);
+
+	/**
+	 * Gets the by id.
+	 *
+	 * @param id the id
+	 * @return the by id
+	 */
 	public Order getById(int id);
-	public void update(Order order);	
+
+	/**
+	 * Update.
+	 *
+	 * @param order the order
+	 */
+	public void update(Order order);
+
+	/**
+	 * Delete.
+	 *
+	 * @param order the order
+	 */
 	public void delete(Order order);
 
 }
