a0116c5e6654ea5dc5f659e3ede6b9c041460a39
sanjana <sanjanagoyal21@gmail.com>
2016-07-01 10:27:42 +0530

    added dishDAO

--- src/main/java/com/foodybuddy/dao/DishDAO.java
+++ src/main/java/com/foodybuddy/dao/DishDAO.java
@@ -4,12 +4,55 @@ import java.util.List;
 
 import com.foodybuddy.model.Dish;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Interface DishDAO.
+ */
 public interface DishDAO {
 
-	public void insert(Dish d);
-	public void update(Dish d);
+	/**
+	 * Insert.
+	 *
+	 * @param d the d
+	 * @return the dish
+	 */
+	public Dish insert(Dish d);
+	
+	/**
+	 * Update.
+	 *
+	 * @param d the d
+	 * @return the dish
+	 */
+	public Dish update(Dish d);
+	
+	/**
+	 * Delete.
+	 *
+	 * @param id the id
+	 */
 	public void delete(Integer id);
-	public List<Dish> getListdish();
-	public Dish getListByDishId(Integer DishId);
+	
+	/**
+	 * Gets the listdish.
+	 *
+	 * @return the listdish
+	 */
+	public List<Dish> getdishList();
+	
+	/**
+	 * Gets the list by dish id.
+	 *
+	 * @param DishId the dish id
+	 * @return the list by dish id
+	 */
+	public Dish getByDishId(Integer DishId);
+	
+	/**
+	 * Gets the list by seller id.
+	 *
+	 * @param SellerId the seller id
+	 * @return the list by seller id
+	 */
 	public List<Dish> getListBySellerId(Integer SellerId);
 }
