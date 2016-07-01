592e9122f58e77526dff0942ed736a87b7f5868b
sanjanagyl <sanjanagoyal21@gmail.com>
2016-07-01 07:20:01 +0530

    added Comments

--- src/main/java/com/foodybuddy/dao/impl/DishDAOImpl.java
+++ src/main/java/com/foodybuddy/dao/impl/DishDAOImpl.java
@@ -1,39 +1,73 @@
 package com.foodybuddy.dao.impl;
+
 import java.util.List;
 import org.hibernate.Session;
 import com.foodybuddy.dao.DishDAO;
 import com.foodybuddy.model.Dish;
 
+/**
+ * The Class DishDAOImpl.
+ */
 public class DishDAOImpl implements DishDAO {
-	private Session session;
 	
-	public DishDAOImpl(Session session) {
-        this.session = session;
-    }
-	public void insert(Dish d) {
-		session.persist(d);
-	}
-	public void update(Dish Dish ){
-		     session.update(Dish); 
-	}
-	public void delete(Integer id) {
-		Dish Dish = (Dish) session.load(Dish.class, new Integer(id));
+	/** The session. */
+	private Session session;
+
+	/**
+	 * Instantiates a new dish DAO impl.
+	 *
+	 * @param session the session
+	 * @throws NullPointerException the null pointer exception
+	 */
+	public DishDAOImpl(Session session) throws NullPointerException {
+		if (session == null) {
+			throw new NullPointerException("Session is null");
+		}
+		this.session = session;
 	}
 
+	/* (non-Javadoc)
+	 * @see com.foodybuddy.dao.DishDAO#insert(com.foodybuddy.model.Dish)
+	 */
+	public Dish insert(Dish d) {
+		session.persist(d);
+		return d;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.foodybuddy.dao.DishDAO#update(com.foodybuddy.model.Dish)
+	 */
+	public Dish update(Dish Dish) {
+		session.update(Dish);
+		return Dish;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.foodybuddy.dao.DishDAO#getDishList()
+	 */
 	@SuppressWarnings("unchecked")
-	public List<Dish> getListdish() {
+	public List<Dish> getDishList() {
 		List<Dish> DishList = session.createQuery("from Dish").list();
 		return DishList;
 	}
-    public Dish getListByDishId(Integer DishId){
-        String query= "FROM Dish WHERE id = " + DishId;
-        Dish dish = (Dish) session.createQuery(query).list().get(0);
-        return dish;    
-    }
+
+	/* (non-Javadoc)
+	 * @see com.foodybuddy.dao.DishDAO#getListByDishId(java.lang.Integer)
+	 */
+	public Dish getByDishId(Integer DishId) {
+		String query = "FROM Dish WHERE id = " + DishId;
+		Dish dish = (Dish) session.createQuery(query).uniqueResult();
+		return dish;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.foodybuddy.dao.DishDAO#getBySellerId(java.lang.Integer)
+	 */
 	@SuppressWarnings("unchecked")
-    public List<Dish> getListBySellerId(Integer sellerid){
-        String query= "FROM Dish WHERE aseller_id = " + sellerid;
-        List<Dish> Dish_by_seller_id = session.createQuery(query).list();
-        return Dish_by_seller_id;    
-    }
+	public List<Dish> getListBySellerId(Integer sellerid) {
+		String query = "FROM Dish WHERE seller_id = " + sellerid;
+		List<Dish> Dish_by_seller_id = session.createQuery(query).list();
+		return Dish_by_seller_id;
+	}
+
 }
