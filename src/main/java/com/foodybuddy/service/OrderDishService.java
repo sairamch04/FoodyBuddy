01219ed3f454c7bc9a8ca20040ef9b1e31d4ccae
junzy <junzy1994@gmail.com>
2016-07-03 11:49:40 +0530

    Updated OrderdDishservice.java

--- src/main/java/com/foodybuddy/service/OrderDishService.java
+++ src/main/java/com/foodybuddy/service/OrderDishService.java
@@ -2,10 +2,14 @@ package com.foodybuddy.service;
 
 import java.util.List;
 
+import com.foodybuddy.model.Dish;
+import com.foodybuddy.model.Order;
 import com.foodybuddy.model.OrderDish;
 
 public interface OrderDishService {
 	public OrderDish updateOrderDishQuantity(int orderDishId, int updatedQuantity) throws Exception ;
-	public List<OrderDish> listOrderByDishId(int dishId) throws Exception ;
-	public List<OrderDish> listOrderByOrderId(int orderId) throws Exception ;
+	public List<OrderDish> getListOrderByDishId(int dishId) throws Exception ;
+	public List<OrderDish> getListOrderByOrderId(int orderId) throws Exception ;
+	public OrderDish insertOrderDish(Order order, Dish dish) throws Exception ;
+
 }
